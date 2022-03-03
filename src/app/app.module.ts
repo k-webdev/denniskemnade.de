4dd1b830ff1bf8716c843079d63637ba49989e18
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-03 17:38:50 +0100

    addet Button designs and menu with logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
