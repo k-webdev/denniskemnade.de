6ad0d438a584034df6ea06304886729635cded7c
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-06 18:14:23 +0100

    fehler beheben

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.openCloseMenu();
+  }
 
   ngOnInit(): void {
+  }
+
+  openCloseCounter = false;
+
+
+  openCloseMenu() {
+    if (this.openCloseCounter == false) {
+      this.openCloseCounter = true;
+    }else if(this.openCloseCounter == true){
+      this.openCloseCounter = false;
+    }
+
+    console.log(this.openCloseCounter);
   }
 
 }
