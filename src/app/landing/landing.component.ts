05d746b031af61e11ac64e3678caca45357854b9
1987DK1987 <87419838+1987DK1987@users.noreply.github.com>
2022-03-23 12:42:07 +0100

    addet textanimation in landing area

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,30 +5,57 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit  {
+export class LandingComponent implements OnInit {
 
   visibleAngular = true;
   visibleJavaScript = true;
+  animateGreetingLine1 = false;
+  animateGreetingLine2 = false;
+  animateGreetingLine3 = false;
+  animateContactButton = false;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.animateGreetingLineOne();
+    this.animateGreetingLineTwo();
+    this.animateGreetingLineThree();
   }
 
-  showAllProjects(){
+  animateGreetingLineOne(){
+    this.animateGreetingLine1 = true;
+  }
+
+  animateGreetingLineTwo(){
+    setTimeout(()=>{
+      this.animateGreetingLine2 = true;
+    },1000);
+  }
+
+  animateGreetingLineThree(){
+    setTimeout(()=>{
+      this.animateGreetingLine3 = true;
+    },3000);
+  }
+
+  showAllProjects() {
     this.visibleAngular = true;
     this.visibleJavaScript = true;
   }
-  
 
-  showElementAngular(){
+
+  showElementAngular() {
     this.visibleAngular = true;
     this.visibleJavaScript = false;
   }
 
-  showElementJavaScript(){
+  showElementJavaScript() {
     this.visibleJavaScript = true;
     this.visibleAngular = false;
   }
+
+  
 }
+
+
