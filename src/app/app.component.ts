12b5c8db785b1df9abcc12db10b045213f782082
Dennis Kemnade <Dennis.Kemnade@live.de>
2022-03-30 10:13:47 +0200

    IntersectionObserver config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,10 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  
   backToTopOpenClose = false;
 
   ngOnInit(){
     window.addEventListener('scroll', this.scrollevent, true);
+    this.classesToAnimate();
   }
 
   scrollevent = (event: any): void =>{
@@ -22,6 +25,70 @@ export class AppComponent {
 
     /* console.log('scroll y: ', window.screenY); */
   }
+
+
+  
+
+    /* ================================ Use intersectionObserver for start and stop animations ================================ */
+
+    classesToAnimate() {
+      setInterval(() => {
+        this.loadAnimationsOnViewport('.rect');
+        this.loadAnimationsOnViewport('.sort-text-img');
+      }, 100);
+    }
+  
+  
+    loadAnimationsOnViewport(classesToCheck) {
+
+      let componentName = classesToCheck;
+  
+      let checkedClass = classesToCheck;
+  
+      const kofiguration = {
+        threshold: [0, 0.75, 1]//"0" ist der Standartwert, 0.75 gibt an wie viel prozent sichtbar sein sollen(hier 75%).
+      };
+  
+      const observer = new IntersectionObserver(
+        (elemente) => {
+         /*  console.log(elemente); */ //kann zur Anzeige der Elemente in der Konsole genutzt werden.
+          elemente.forEach((element) => {
+  
+            if (element.intersectionRatio == 1 && element.intersectionRatio > 0.75) {// wenn das element mindestens 75% im sichtfeld liegt. (0.75 klappt nur wenn threshold den Wert kennt).
+              if(componentName == '.rect'){  //my skills
+              element.target.classList.add('animationSlideIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
+                element.target.classList.remove('animationSlideOut');
+              }else if(componentName == '.sort-text-img'){ //my work
+                element.target.classList.add('my-skills-animationIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
+                element.target.classList.remove('my-skills-animationOut');
+              }
+
+            } else if(element.intersectionRatio < 0.75){
+                if(componentName == '.rect'){ //my skills
+                  element.target.classList.remove('animationSlideIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
+                element.target.classList.add('animationSlideOut');
+                }else if(componentName == '.sort-text-img'){  //my work
+                  element.target.classList.remove('my-skills-animationIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
+                  element.target.classList.add('my-skills-animationOut');
+                }
+                
+            }
+  
+          });
+        }, kofiguration
+      );
+  
+      const animatedElements = document.querySelectorAll(`${checkedClass}`);// Alle Elemente mit der Klasse "test" werden beobachtet
+      this.elementsToCheck(animatedElements, observer);
+    }
+  
+    elementsToCheck(animatedElements, observer) {
+      animatedElements.forEach((element) => {
+        observer.observe(element);
+      });
+    }
+  
+    /* ============================== Use intersectionObserver for start and stop animations end ============================== */
 
 
   
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,3 @@
-import { ViewportScroller } from '@angular/common';
-import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -14,11 +12,11 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
 
   
+  inViewPort = true;//Animatet Button contact me
   animateGreetingLine1 = false;
   animateGreetingLine2 = false;
   animateGreetingLine3 = false;
   animateButtonOnOff = false;
-  inViewPort = true;//Animatet Button contact me
 
   constructor() {
   }
@@ -28,10 +26,7 @@ export class LandingComponent implements OnInit {
     this.animateGreetingLineTwo();
     this.animateGreetingLineThree();
     this.animateButton();
-    this.classesToAnimate();
   }
-
-  
 
   animateGreetingLineOne() {//animates "Hi," on landing top 
     this.animateGreetingLine1 = true;
@@ -61,60 +56,6 @@ export class LandingComponent implements OnInit {
       }
     }, 3000);
   }
-  /* ================================ Use intersectionObserver for start and stop animations ================================ */
 
-  classesToAnimate() {
-    setInterval(() => {
-      this.loadAnimationsOnViewport('.rect');
-      this.loadAnimationsOnViewport('.sort-text-img');
-    }, 100);
-  }
-
-
-  loadAnimationsOnViewport(classesToCheck) {
-
-    let checkedClass = classesToCheck;
-
-    const kofiguration = {
-      threshold: [0, 0.75, 1]//"0" ist der Standartwert, 0.75 gibt an wie viel prozent sichtbar sein sollen(hier 75%).
-    };
-
-    const observer = new IntersectionObserver(
-      (elemente) => {
-        /* console.log(elemente); */ //kann zur Anzeige der Elemente in der Konsole genutzt werden.
-        elemente.forEach((element) => {
-
-          if (element.intersectionRatio == 1 && element.intersectionRatio > 0.75) {// wenn das element mindestens 75% im sichtfeld liegt. (0.75 klappt nur wenn threshold den Wert kennt).
-            if (classesToCheck == '.sort-text-img') {
-              element.target.classList.add('my-skills-animationIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
-              element.target.classList.remove('my-skills-animationOut');
-            } if (classesToCheck == '.rect') {
-              element.target.classList.add('animationSlideIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
-              element.target.classList.remove('animationSlideOut');
-            }
-          } else if(element.intersectionRatio < 0.75){
-            if (classesToCheck == '.sort-text-img') {
-              element.target.classList.remove('my-skills-animationIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
-              element.target.classList.add('my-skills-animationOut');
-            } if (classesToCheck == '.rect') {
-              element.target.classList.remove('animationSlideIn');//hinzufügen der klasse "sichtbar", welche die Animation beinhaltet.
-              element.target.classList.add('animationSlideOut');
-            }
-          }
-
-        });
-      }, kofiguration
-    );
-
-    const animatedElements = document.querySelectorAll(`${checkedClass}`);// Alle Elemente mit der Klasse "test" werden beobachtet
-    this.elementsToCheck(animatedElements, observer);
-  }
-
-  elementsToCheck(animatedElements, observer) {
-    animatedElements.forEach((element) => {
-      observer.observe(element);
-    });
-  }
-
-  /* ============================== Use intersectionObserver for start and stop animations end ============================== */
+ 
 }
